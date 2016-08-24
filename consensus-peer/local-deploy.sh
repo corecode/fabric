18d35cc36c2546b9aa537b0a6e585b1a7c8f1e14
Simon Schubert <sis@zurich.ibm.com>
2016-08-24 11:52:01 +0200

    adapt local-deploy for json-based config
    Signed-off-by: Simon Schubert <sis@zurich.ibm.com>

--- consensus-peer/local-deploy.sh
+++ consensus-peer/local-deploy.sh
@@ -20,6 +20,8 @@ cd $dest
 certtool --generate-privkey --outfile key.pem 2>/dev/null
 mkdir data
 
+peerconf=""
+
 for n in $(seq $count)
 do
     cat > template$n.cfg <<EOF
@@ -30,15 +32,38 @@ encryption_key
 EOF
     certtool --generate-self-signed --load-privkey key.pem --outfile cert$n.pem --template template$n.cfg 2>/dev/null
     certtool -i --infile cert$n.pem --outder --outfile data/config.peers.:$((6100+$n)) 2>/dev/null
+
+    peerconf=$(cat <<EOF
+${peerconf}${peerconf:+,}
+{
+  "address": ":$((6100+$n))",
+  "cert": "cert${n}.pem"
+}
+EOF
+)
 done
+
+cat > config.json <<EOF
+{
+  "consensus": {
+    "pbft_config": {
+      "f": $fail,
+      "k": 2,
+      "l_multiplier": 2,
+      "request_timeout": 2.0,
+      "viewchange_resend_timeout": 2.0,
+      "viewchange_timeout": 2.0
+    }
+  },
+  "peers": [${peerconf}]
+}
+EOF
 
 for n in $(seq $count)
 do
-    cp -R data data$n
+    consensus-peer -data-dir data$n -init config.json
     cat > run-$n.sh <<EOF
 #!/bin/sh
-export CORE_PBFT_GENERAL_N=$count
-export CORE_PBFT_GENERAL_F=$fail
 consensus-peer -addr :$((6100+$n)) -cert cert$n.pem -key key.pem -data-dir data$n "\$@"
 EOF
     chmod +x run-$n.sh
