28c393fdcff2ff436e727cec57e3fff10e9cc120
Simon Schubert <sis@zurich.ibm.com>
2016-09-26 15:02:22 +0200

    add performance benchmark for rsa
    Signed-off-by: Simon Schubert <sis@zurich.ibm.com>

--- consensus/simplebft/testsys_test_test.go
+++ consensus/simplebft/testsys_test_test.go
@@ -17,9 +17,12 @@ limitations under the License.
 package simplebft
 
 import (
+	"crypto"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
 	"testing"
 	"time"
 )
@@ -50,12 +53,44 @@ func TestSys(t *testing.T) {
 // 	}
 // }
 
-func BenchmarkEcdsa(b *testing.B) {
+func BenchmarkEcdsaSign(b *testing.B) {
 	key, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	val := make([]byte, 32, 32)
+	val := sha256.Sum256(make([]byte, 32, 32))
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		ecdsa.Sign(rand.Reader, key, val)
+		ecdsa.Sign(rand.Reader, key, val[:])
+	}
+}
+
+func BenchmarkRsaSign(b *testing.B) {
+	key, _ := rsa.GenerateKey(rand.Reader, 2048)
+	val := sha256.Sum256(make([]byte, 32, 32))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		rsa.SignPSS(rand.Reader, key, crypto.SHA256, val[:], nil)
+	}
+}
+
+func BenchmarkEcdsaVerify(b *testing.B) {
+	key, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	val := sha256.Sum256(make([]byte, 32, 32))
+	r, s, _ := ecdsa.Sign(rand.Reader, key, val[:])
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ecdsa.Verify(&key.PublicKey, val[:], r, s)
+	}
+}
+
+func BenchmarkRsaVerify(b *testing.B) {
+	key, _ := rsa.GenerateKey(rand.Reader, 2048)
+	val := sha256.Sum256(make([]byte, 32, 32))
+	sig, _ := rsa.SignPSS(rand.Reader, key, crypto.SHA256, val[:], nil)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		rsa.VerifyPSS(&key.PublicKey, crypto.SHA256, val[:], sig, nil)
 	}
 }
